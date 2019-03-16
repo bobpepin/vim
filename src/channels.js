46371e2dc96d36b1673c8f75387aa4a353f45b4d
Bob Pepin <bobpepin@users.noreply.github.com>
2019-03-16 15:17:18 +0100

    Add Vim channels interface based on ES2017 async/await. Add Typescript support.

---
+++ src/channels.js
@@ -0,0 +1,53 @@
+// 1. Accessible by the Vim GC: Dictionary id -> channel obj
+// 2. Wrapper for open channel, ch_open -> returns id
+// 3. Read function: if data in buffer, resolve with buffer. 
+//    Else register callback with resolve
+
+require('promise.js')
+
+var callbacks = {}
+function set_callback(channel, fun) {
+    callbacks[channel.id] = fun
+    vim_eval("Duk_ch_enable_cb(" + channel.id + ")")
+}
+exports.set_callback = set_callback
+
+function callback(channel_id, channel_msg, status) {
+    msg("Channel " + channel_id + " (" + status + ")" + ": " + channel_msg)
+    vim_eval("Duk_ch_disable_cb(" + channel_id + ")")
+    var fun = callbacks[channel_id]
+    delete callbacks[channel_id]
+    fun(channel_id, channel_msg, status)
+}
+
+globalThis.channel_callback = callback
+
+function open(address) {
+    var channel_id = vim_eval("Duk_ch_open('" + address + "')")
+    if(channel_id == -1) {
+        throw Error("Connection to " + address + " failed.");
+    }
+    var channel = {id: channel_id}
+    Duktape.fin(channel, close)
+    return channel
+}
+
+exports.open = open
+
+function close(channel) {
+    vim_eval("Duk_ch_close(" + channel.id + ")")
+}
+
+exports.close = close
+
+function read(channel) {
+    var p = new Promise(function (resolve, reject) {
+        set_callback(channel, function (ch_id, ch_msg, status) {
+            resolve(ch_msg);
+            Promise.runQueue();
+        })
+    })
+    return p;
+}
+
+exports.read = read
